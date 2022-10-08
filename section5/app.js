edef0622571dbf6b18eeb420b81a4ad3a2243c1e
melitina915 <90691824+melitina915@users.noreply.github.com>
2022-10-09 06:46:39 +0900

    Create app.js

---
+++ section5/app.js
@@ -0,0 +1,40 @@
+var express = require('express'); //express라는 모듈을 load 한다
+var app = express();
+app.use(express.static('public'));
+app.get('/', function(req, res){
+    res.send('Hello home page');
+})
+app.get('/dynamic', function(req, res){
+    var lis = '';
+    for(var i = 0; i < 5; i++){
+        lis = lis + '<li>coding</li>';
+    }
+    var time = Date();
+    var output = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        Hello, Dynamic!
+        <ul>
+            ${lis}
+        </ul>
+        ${time}
+    </body>
+    </html>`
+    res.send(output);
+})
+app.get('/route', function(req, res){
+    res.send('Hello Router, <img src="/route.png">')
+})
+app.get('/login', function(req, res){
+    res.send('<h1>Login please</h1>');
+})
+app.listen(3000, function(){
+    console.log('Connected 3000 port!');
+});
