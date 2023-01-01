b9c90dd46b48a61e370b250e8109794e8107c19d
melitina915 <90691824+melitina915@users.noreply.github.com>
2023-01-02 02:41:42 +0900

    Update server.js

--- Part3/server.js
+++ Part3/server.js
@@ -68,36 +68,6 @@ app.get('/write', function(req, res){
     res.sendFile(__dirname + '/write.html');
 });
 
-// 작성 기능
-app.post('/add', function(req, res){
-    res.send('전송완료');
-    //console.log(req.body)
-    console.log(req.body.title)
-    console.log(req.body.date)
-
-    // 어떤 사람이 /add 라는 경로로 post 요청을 하면,
-    // 데이터 2개(날짜, 제목)을 보내주는데,
-    // 이 때, 'post'라는 이름을 가진 collection에 두 개 데이터를 저장
-    // { 제목 : '어쩌구', 날짜 : '어쩌구' }
-    db.collection('counter').findOne({name : '게시물 개수'}, function(error, result){
-        console.log(result.totalPost);
-        var totalpostnum = result.totalPost;
-
-        // 글을 발행해주는 기능
-        db.collection('post').insertOne({ _id : totalpostnum + 1, title : req.body.title, date : req.body.date }, function(error, result){
-            // _id : 총 게시물 개수 + 1
-            console.log('저장완료');
-
-            // counter라는 콜렉션에 있는 totalPost라는 항목도 1 증가시켜야 한다
-            db.collection('counter').updateOne({name : '게시물 개수'}, { $inc : {totalPost : 1} }, function(error, result){
-                if(error){
-                    return console.log('에러')
-                }
-            });
-        });
-    });
-});
-
 
 
 // 글 번호 달기 기능
@@ -122,29 +92,30 @@ app.get('/list', function(req, res){
 
 // 서버에서 query string 꺼내는 법
 app.get('/search', (req, res) => {
+    var searching = [
+        {
+            $search: {
+                index: 'titleSearch',
+                text: {
+                    query: req.query.value,
+                    path: 'title' // 제목 날짜 둘 다 찾고 싶으면 ['제목', '날짜']
+                }
+            }
+        }, 
+        //{ $sort : {_id : 1 } }, // 검색조건 더 주어 검색 결과 정렬
+        //{ $limit : 10 } // 개수 limit 주기
+        //{ $project : { title: 1, _id: 0, score: { $meta: "searchScore" } } } // 검색 결과에서 필터 주기
+        // 위 조건은 title을 가져오고 id는 가져오지 않고 score 항목을 추가하여 점수를 가져온다
+    ]
     console.log(req.query.value); // req에는 요청한 유저의 정보가 다 담겨있다
     // req.query.value는 { object 자료 }에서 데이터를 꺼내는 문법일 뿐이다
-    db.collection('post').find({title:req.query.value}).toArray((err, result) => {
+//    db.collection('post').find({title:req.query.value}).toArray((err, result) => {
+//    db.collection('post').find( { $text: { $search: req.query.value } } ).toArray((err, result) => {
+    db.collection('post').aggregate(searching).toArray((err, result) => {
         // 제목이 정확히 req.query.value와 일치하는 것만 찾아준다
         console.log(result);
         // ejs 파일에 데이터 보내기
         res.render('search.ejs', {posts : result});
-    });
-});
-
-
-
-app.delete('/delete', function(req, res){
-    console.log(req.body);
-    req.body._id = parseInt(req.body._id) //문자형 '1'을 정수 1로 변환
-    //req.body에 담겨온 게시물 번호를 가진 글을 db에서 찾아서 삭제해주세요
-    db.collection('post').deleteOne(req.body, function(error, result){
-        // 터미널 창에 삭제 완료 출력
-        console.log('삭제 완료');
-
-        //응답코드 200을 보내고 메시지를 보내주세요
-        res.status(200).send({ message : '성공' });
-        //res.status(400).send({ message : '실패' });
     });
 });
 
@@ -277,7 +248,66 @@ passport.deserializeUser(function(userid, done){
 
 
 
+app.post('/register', function(req, res){
+    db.collection('login').insertOne( {id : req.body.id, pw : req.body.pw }, function(err, result){
+        res.redirect('/');
+    } );
+});
 
 
 
+// 작성 기능
+app.post('/add', function(req, res){
+    res.send('전송완료');
+    //console.log(req.body)
+    console.log(req.body.title)
+    console.log(req.body.date)
 
+    // 어떤 사람이 /add 라는 경로로 post 요청을 하면,
+    // 데이터 2개(날짜, 제목)을 보내주는데,
+    // 이 때, 'post'라는 이름을 가진 collection에 두 개 데이터를 저장
+    // { 제목 : '어쩌구', 날짜 : '어쩌구' }
+    db.collection('counter').findOne({name : '게시물 개수'}, function(error, result){
+        console.log(result.totalPost);
+        var totalpostnum = result.totalPost;
+
+        var tosave = { _id : totalpostnum + 1, editor : req.user._id, title : req.body.title, date : req.body.date }
+
+        // 글을 발행해주는 기능
+        db.collection('post').insertOne(tosave, function(error, result){
+            // _id : 총 게시물 개수 + 1
+            console.log('저장완료');
+
+            // counter라는 콜렉션에 있는 totalPost라는 항목도 1 증가시켜야 한다
+            db.collection('counter').updateOne({name : '게시물 개수'}, { $inc : {totalPost : 1} }, function(error, result){
+                if(error){
+                    return console.log('에러')
+                }
+            });
+        });
+    });
+});
+
+
+
+app.delete('/delete', function(req, res){
+    console.log('삭제 요청');
+    console.log(req.body);
+    req.body._id = parseInt(req.body._id); //문자형 '1'을 정수 1로 변환
+    
+    // _id와 작성자가 일치하는 게시물을 찾아서 지워준다
+    var deletedata = { _id : req.body._id, editor : req.user._id };
+    
+    //req.body에 담겨온 게시물 번호를 가진 글을 db에서 찾아서 삭제해주세요
+    db.collection('post').deleteOne(deletedata, function(error, result){
+        // 터미널 창에 삭제 완료 출력
+        console.log('삭제 완료');
+
+        if(error) {console.log(error);}
+        if(result) {console.log(result);}
+
+        //응답코드 200을 보내고 메시지를 보내주세요
+        res.status(200).send({ message : '성공' });
+        //res.status(400).send({ message : '실패' });
+    });
+});
